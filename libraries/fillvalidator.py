2138a92ed1863472b9cfbad4389d324616595b9c
mitzvah capital <munair@mitzvah.capital>
2020-10-05 09:53:58 -0500

    my bad... put it on the wrong routine because i can't read.

--- libraries/fillvalidator.py
+++ libraries/fillvalidator.py
@@ -24,8 +24,8 @@ def confirmexecution(
     logger.debug(f'Confirming exection of the order identified by the Gemini assigned number: {orderid}')
 
     # Define websocet functions.
-    def on_close(ws): logger.debug(f'{ws} connection closed.')
-    def on_open(ws, exitstatus=exitstatus): logger.debug(f'{ws} connection opened.'); return exitstatus
+    def on_close(ws, exitstatus=exitstatus): logger.debug(f'{ws} connection closed.'); return exitstatus
+    def on_open(ws): logger.debug(f'{ws} connection opened.')
     def on_error(ws, error): logger.debug(error)
     def on_message(ws, message, orderid=orderid):
         dictionary = json.loads( message )
