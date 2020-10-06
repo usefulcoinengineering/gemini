f33d23dce660581b04c46233c5a95bb1adc3405d
mitzvah capital <munair@mitzvah.capital>
2020-10-06 00:35:02 -0500

    add sell side.

--- libraries/spreadkiller.py
+++ libraries/spreadkiller.py
@@ -27,12 +27,6 @@ def bidorder(
     tick = Decimal( zeros  + '1' ) * 10
     bid = str( market - Decimal( tick ).quantize( market ) )
 
-    logger.debug(f'endpoint: {endpoint}')
-    logger.debug(f'market: {market}')
-    logger.debug(f'zeros: {zeros}')
-    logger.debug(f'tick: {tick}')
-    logger.debug(f'bid: {bid}')
-
     # Construct buy order payload.
     # Use 'options': ['maker-or-cancel'] for post only orders.
     endpoint = '/v1/order/new'
@@ -43,6 +37,40 @@ def bidorder(
         'symbol': pair,
         'amount': size,
         'price': bid,
+        'side': 'buy',
+        'type': 'exchange limit',
+        'options': ['maker-or-cancel']
+    }
+    headers = authenticator.authenticate(payload)
+
+    request = resourcelocator.restserver + endpoint
+    response = requests.post(request, data = None, headers = headers['restheader'])
+
+    return response
+
+def askorder(
+        pair: str,
+        size: str,
+    ) -> None:
+
+    # Get the lowest ask in the orderbook.
+    endpoint = '/v1/pubticker/' + pair
+    response = requests.get( resourcelocator.restserver + endpoint )
+    market = Decimal( response.json()['bid'] )
+    zeros = str( Decimal( 0 ).quantize( market ) )
+    tick = Decimal( zeros  + '1' ) * 10
+    ask = str( market + Decimal( tick ).quantize( market ) )
+
+    # Construct buy order payload.
+    # Use 'options': ['maker-or-cancel'] for post only orders.
+    endpoint = '/v1/order/new'
+    t = datetime.datetime.now()
+    payload = {
+        'request': endpoint,
+        'nonce': str(int(time.mktime(t.timetuple())*1000)),
+        'symbol': pair,
+        'amount': size,
+        'price': ask,
         'side': 'buy',
         'type': 'exchange limit',
         'options': ['maker-or-cancel']
