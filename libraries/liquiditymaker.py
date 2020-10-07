c90f878fb6c77ea2d3e6b1e2a560c10b5d811c2e
mitzvah capital <munair@mitzvah.capital>
2020-10-06 19:23:52 -0500

    add module for quota buy.

--- libraries/liquiditymaker.py
+++ libraries/liquiditymaker.py
@@ -41,6 +41,46 @@ def bidorder(
 
     return response
 
+def quotabid(
+        pair: str,
+        cash: str,
+        cost: str
+    ) -> None:
+
+    # Determine API transaction fee.
+    # Refer to https://docs.gemini.com/rest-api/#basis-point.
+    # Fees are calculated on the notional value of each trade (price × size).
+    # Meaning (for API transactions): size * price * 1.001 = cash
+    fraction = 0.001
+    notional = Decimal(cash) / Decimal( 1 + fraction )
+
+    # Determine bid size.
+    quantity = notional / Decimal(cost)
+    size = str( quantity.quantize( Decimal('1.000') ) )
+    logger.debug(f'size: {size}')
+    logger.debug(f'cost: {cost}')
+
+    # Construct buy order payload.
+    # Use 'options': ['maker-or-cancel'] for post only orders.
+    endpoint = '/v1/order/new'
+    t = datetime.datetime.now()
+    payload = {
+        'request': endpoint,
+        'nonce': str(int(time.mktime(t.timetuple())*1000)),
+        'symbol': pair,
+        'amount': size,
+        'price': str(cost),
+        'side': 'buy',
+        'type': 'exchange limit',
+        'options': ['maker-or-cancel']
+    }
+    headers = authenticator.authenticate(payload)
+
+    request = resourcelocator.restserver + endpoint
+    response = requests.post(request, data = None, headers = headers['restheader'])
+
+    return response
+
 def askorder(
         pair: str,
         size: str,
