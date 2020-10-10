8b68f876db61b774f50ccdb0c985033f33ded66b
mitzvah capital <munair@mitzvah.capital>
2020-10-10 14:30:49 -0500

    big experiment!

---
+++ strategies/skimyfiusd.bash
@@ -0,0 +1,12 @@
+#! /bin/bash
+#
+# script name: skimyfiusd.bash
+# script author: munair simpson
+# script created: 20201010
+# script purpose: bid and ask on YFI using USD on quota
+
+# LONG on YFI and accumulating YFI:
+while [[ True ]]; do
+  python3 ../bidyfiusddiscount.py
+  python3 ../askyfiusddiscount.py
+done
