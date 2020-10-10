9857f71624649052d026d00043adb67c0ebf8abf
mitzvah capital <munair@mitzvah.capital>
2020-10-10 13:19:23 -0500

    retire strategy. time for something new.

--- strategies/bidyfiusddiscount.py
+++ strategies/bidyfiusddiscount.py
@@ -2,12 +2,12 @@
 
 
 # Strategy Outline:
-#  1. Waiting for a drop in the price of YFI.
-#  2. Submit a bid one tick below the best ask.
-#  3. The bid size is limited by a USD (quote currency) budget.
+#  1. Waiting for a rise in the price of YFI.
+#  2. Submit an ask one tick above the best bid.
+#  3. The ask size is limited by a USD (quote currency) budget.
 #
 # Execution:
-#   - Copy this file from the strategies directory to the level below. Run with python3.
+#   - Copy this file from the strategies directory to the level above. Run with python3.
 
 import json
 import requests
@@ -15,28 +15,28 @@ import requests
 from decimal import Decimal
 
 from libraries.logger import logger
-from libraries.dealseeker import askfall
-from libraries.spreadkiller import quotabid
+from libraries.rentseeker import bidrise
+from libraries.spreadkiller import quotaask
 from libraries.fillvalidator import confirmexecution
 
 
 # Set quote currency (USD in this case) budget.
 # This amount should exceed 20 cents ['0.00001' is the minimum for YFIUSD].
-# Configure price drop desired in decimal terms.
+# Configure price rise desired in decimal terms.
 # For example, 20 basis points is '0.002'. This covers Gemini API trading fees round trip!
 pair = 'YFIUSD'
 cash = '0.25'
-drop = '0.001'
+rise = '0.001'
 
 # Open websocket connection.
-# Wait for asks to fall in price.
-logger.info(f'waiting for {pair} to drop {Decimal(drop)*100}% in price to buy {cash} {pair[3:]} worth..')
-deal = askfall( pair, drop )
+# Wait for bids to rise in price.
+logger.info(f'waiting for {pair} to rise {Decimal(rise)*100}% in price to buy {cash} {pair[3:]} worth..')
+deal = bidrise( pair, rise )
 if deal:
 
-    # Submit limit bid order.
-    logger.debug(f'submitting {pair} aggressive limit bid order.')
-    post = quotabid( pair, cash )
+    # Submit limit ask order.
+    logger.debug(f'submitting {pair} aggressive limit ask order.')
+    post = quotaask( pair, cash )
     post = post.json()
     dump = json.dumps( post, sort_keys=True, indent=4, separators=(',', ': ') )
     logger.debug ( dump )
