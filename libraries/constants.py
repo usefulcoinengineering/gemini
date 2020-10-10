5c3207c19f7c6ae642bdfe2ad63bc7588bf4fe2e
mitzvah capital <munair@mitzvah.capital>
2020-10-09 22:06:52 -0500

    Add constants that the Gemini API should be able to serve up!

---
+++ libraries/constants.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+
+
+apitransactionfee = '0.001'
+
+ticksizes = [
+ {
+   'currency': 'BTC',
+   'tick': '0.00000001'
+ },
+ {
+   'currency': 'ETH',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'ZEC',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'BCH',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'LTC',
+   'tick': '0.00001'
+ },
+ {
+   'currency': 'BAT',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'DAI',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'LINK',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'OXT',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'AMP',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'COMP',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'PAXG',
+   'tick': '0.00000001'
+ },
+ {
+   'currency': 'MKR',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'ZRX',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'KNC',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'MANA',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'STORJ',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'SNX',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'CRV',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'BAL',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'UNI',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'REN',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'UMA',
+   'tick': '0.000001'
+ },
+ {
+   'currency': 'YFI',
+   'tick': '0.00000001'
+ },
+ {
+   'currency': 'AAVE',
+   'tick': '0.000001'
+ }
+]
+
+minimumorders = [
+ {
+   'currency': 'BTC',
+   'minimumorder': '0.00001'
+ },
+ {
+   'currency': 'ETH',
+   'minimumorder': '0.001'
+ },
+ {
+   'currency': 'ZEC',
+   'minimumorder': '0.001'
+ },
+ {
+   'currency': 'BCH',
+   'minimumorder': '0.001'
+ },
+ {
+   'currency': 'LTC',
+   'minimumorder': '0.01'
+ },
+ {
+   'currency': 'BAT',
+   'minimumorder': '1.0'
+ },
+ {
+   'currency': 'DAI',
+   'minimumorder': '0.1'
+ },
+ {
+   'currency': 'LINK',
+   'minimumorder': '0.1'
+ },
+ {
+   'currency': 'OXT',
+   'minimumorder': '1.0'
+ },
+ {
+   'currency': 'AMP',
+   'minimumorder': '10.0'
+ },
+ {
+   'currency': 'COMP',
+   'minimumorder': '0.001'
+ },
+ {
+   'currency': 'PAXG',
+   'minimumorder': '0.0001'
+ },
+ {
+   'currency': 'MKR',
+   'minimumorder': '0.001'
+ },
+ {
+   'currency': 'ZRX',
+   'minimumorder': '0.1'
+ },
+ {
+   'currency': 'KNC',
+   'minimumorder': '0.1'
+ },
+ {
+   'currency': 'MANA',
+   'minimumorder': '1.0'
+ },
+ {
+   'currency': 'STORJ',
+   'minimumorder': '0.1'
+ },
+ {
+   'currency': 'SNX',
+   'minimumorder': '0.01'
+ },
+ {
+   'currency': 'CRV',
+   'minimumorder': '0.1'
+ },
+ {
+   'currency': 'BAL',
+   'minimumorder': '0.01'
+ },
+ {
+   'currency': 'UNI',
+   'minimumorder': '0.01'
+ },
+ {
+   'currency': 'REN',
+   'minimumorder': '0.01'
+ },
+ {
+   'currency': 'UMA',
+   'minimumorder': '0.01'
+ },
+ {
+   'currency': 'YFI',
+   'minimumorder': '0.00001'
+ },
+ {
+   'currency': 'AAVE',
+   'minimumorder': '0.001'
+ }
+]
