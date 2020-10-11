249c2209fbc8e97713cf0a5f890b809bae52ab2c
mitzvah capital <munair@mitzvah.capital>
2020-10-11 14:08:49 -0500

    redo strategy so that parameter are passed through the command line. the script just wraps the recipe.

--- strategies/skimyfiusd.bash
+++ strategies/skimyfiusd.bash
@@ -7,6 +7,6 @@
 
 # LONG on YFI and accumulating YFI:
 while [[ True ]]; do
-  python3 ../bidyfiusddiscount.py || break
-  python3 ../askyfiusddiscount.py || break
+  python3 ../discountquotabid.py YFIUSD 137.60 0.001 || break
+  python3 ../discountquotaask.py YFIUSD 137.60 0.020 || break
 done
