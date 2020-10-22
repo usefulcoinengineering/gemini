ea7a40224c0364e489c3c0e4c8afe218fb603c19
mitzvah capital <munair@mitzvah.capital>
2020-10-22 15:59:52 -0500

    document parameters

--- strategies/skimyfiusd.bash
+++ strategies/skimyfiusd.bash
@@ -6,6 +6,9 @@
 # script purpose: bid and ask on YFI using USD on quota
 
 # LONG on YFI and accumulating YFI:
+# Parameter 0 is the pair.
+# Parameter 1 is the budget (100 is 100 USD)
+# Parameter 2 is the discount (0.02 is a 2% discount)
 while [[ True ]]; do
   python3 ../discountquotabid.py YFIUSD 137.60 0.02 || break
   python3 ../discountquotaask.py YFIUSD 137.60 0.02 || break
