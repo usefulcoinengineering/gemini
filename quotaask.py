5230897d23f410cecb2dc9fc66c574caf6622355
mitzvah capital <munair@mitzvah.capital>
2020-10-22 15:50:10 -0500

    simple bid/ask request on a budget.

---
+++ quotaask.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+
+# Strategy Outline:
+#  1. Want to sell DAI at a specific price.
+#  2. The ask size is limited by a USD (quote currency) budget.
+#
+# Execution:
+#   - Use the wrapper BASH script in the "strategies" directory.
+
+import sys
+import json
+import requests
+
+from decimal import Decimal
+
+from libraries.logger import logger
+from libraries.liquiditymaker import quotabid
+from libraries.fillvalidator import confirmexecution
+
+
+# Set your price reservation.
+# Set quote currency (USD in this case) budget.
+pair = 'DAIUSD'
+cash = '100'
+cost = '1.00913'
+
+# Override defaults with command line parameters from BASH wrapper.
+if len(sys.argv) == 4:
+    pair = sys.argv[1]
+    cash = sys.argv[2]
+    drop = sys.argv[3]
+
+# Submit limit ask order.
+logger.debug(f'submitting {pair} limit ask order: {cost} ask on a {cost} budget.')
+post = quotaask( pair, cash, cost )
+post = post.json()
+dump = json.dumps( post, sort_keys=True, indent=4, separators=(',', ': ') )
+logger.debug ( dump )
+
+# Define poststatus class.
+# Purpose: Stores the state of the orderid parameter upon exiting the websocket connection session.
+class Poststatus:
+    def __init__(self, state): self.__state = state
+    def getvalue(self): return self.__state
+    def setvalue(self, state): self.__state = state
+
+poststatus = Poststatus('')
+
+# Determine if the order was filled.
+confirmexecution( orderid = post['order_id'], poststatus = poststatus )
+if 'filled' in poststatus.getvalue(): poststatus = True
+
+# Let the shell know we successfully made it this far!
+if poststatus: sys.exit(0)
+else: sys.exit(1)
