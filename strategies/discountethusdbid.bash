dbb5cd937dd3a5fdd45249a174bf8d60285f0a02
mitzvah capital <munair@mitzvah.capital>
2020-10-29 19:47:43 -0500

    Run a test.

--- strategies/discountethusdbid.bash
+++ strategies/discountethusdbid.bash
@@ -10,4 +10,4 @@
 # Parameter 0 is the pair.
 # Parameter 1 is the budget (100 is 100 USD)
 # Parameter 2 is the discount (0.02 is a 2% discount)
-python3 ../discountquotabid.py ETHUSD 3500 0.001 && echo success!
+python3 ../discountquotabid.py ETHUSD 500 0.001 && echo success!
