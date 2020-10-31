240315797607b1940ac59b6922d053f55031819f
mitzvah capital <munair@mitzvah.capital>
2020-10-31 13:24:11 -0500

    let's try to skim btc in the case it really is trending up.

---
+++ strategies/skimbtcusd.bash
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# script name: skimbtcusd.bash
+# script author: munair simpson
+# script created: 20201010
+# script purpose: bid and ask on BTC using USD on quota
+
+# LONG on BTC and accumulating BTC:
+# Parameter 0 is the pair.
+# Parameter 1 is the budget (100 is 100 USD).
+# Parameter 2 is the discount (0.02 is a 2% discount).
+while [[ True ]]; do
+  python3 ../discountquotabid.py BTCUSD 100 0.01 || break
+  python3 ../discountquotaask.py BTCUSD 100 0.01 || break
+done
