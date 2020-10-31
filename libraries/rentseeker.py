b4a499a1ad82c10def682cc17351c109a274d3fe
mitzvah capital <munair@mitzvah.capital>
2020-10-31 12:40:30 -0500

    add logic from improved dealseeker.

--- libraries/rentseeker.py
+++ libraries/rentseeker.py
@@ -11,6 +11,7 @@
 # This is if you want to communicate a short message and disconnect immediately when done.
 
 
+import statistics
 import requests
 import ssl
 import json
@@ -31,15 +32,9 @@ def bidrise (
         rise: str
         ) -> None:
 
-    # Define Cost class.
-    # Purpose: Stores the cost price used to determine gains.
-    class Cost:
-        def __init__(self, price): self.__price = price
-        def getvalue(self): return self.__price
-        def setvalue(self, price): self.__price = price
-
-    # Instantiate Cost object.
-    cost = Cost(0)
+    # Define dataset class.
+    # Purpose: Stores the offers received during the websocket connection session.
+    dataset = []
 
     # Construct subscription request.
     subscriptionrequest = f'{{"type": "subscribe","subscriptions":[{{"name":"l2","symbols":["{pair}"]}}]}}'
@@ -60,7 +55,6 @@ def bidrise (
         newmessage = ws.recv()
         dictionary = json.loads( newmessage )
         grossmargin = Decimal( rise )
-        investment = Decimal( cost.getvalue() )
 
         # Uncomment this statement to debug messages: logger.debug(dictionary)
 
@@ -72,34 +66,43 @@ def bidrise (
                 # Rank bids and determine the highest bid among the changes in the Gemini L2 update response.
                 bidranking = [ Decimal(change[1]) for change in changes if change[0] == 'buy' ]
                 if bidranking != []:
-                    maximumbid = max(bidranking)
+                    maximum = max(bidranking)
+                    dataset.append(maximum)
 
-                    # Define investment.
-                    # Use first maximum.
-                    if investment == 0:
-                        investment = maximumbid
-                        cost.setvalue( maximumbid )
+                    # Define session minimum and average values.
+                    sessionmin = Decimal( min(dataset) )
+                    sessionavg = Decimal( statistics.mean(dataset) )
 
                     # Calculate movement away from cost [if any].
-                    move = 100 * ( maximumbid - investment ) / investment
+                    move = 100 * ( maximum - sessionmin ) / sessionmin
+
+                    # Determine how much the maximum deviates away from the session average.
+                    # If it deviated by more than four standard deviations, then do nothing further.
+                    # Continue at the start of the loop.
+                    deviatedby = maximum - sessionavg
+                    if len(dataset) != 1:
+                        if deviatedby.compare( 4 * statistics.stdev(dataset) ) == 1:
+                            logger.info( f'{move:.2f}% off lows [{sessionmin}] : {pair} is {maximum} presently. Aberration... The mean is: {sessionavg:.2f}. Dumping!' )
+                            dataset.pop()
+                            continue
 
                     # Display impact of event information received.
-                    logger.info( f'{pair} is {maximumbid} presently: {move:.2f}% away from {investment}.' )
+                    logger.info( f'{move:.2f}% off lows [{sessionmin}] : {pair} is {maximum} presently.' )
 
-                    # Define bargain (sale) price.
-                    sale = Decimal( investment * ( 1 + grossmargin ) )
+                    # Define profitable (rent) price.
+                    rent = Decimal( sessionmin * ( 1 + grossmargin ) )
 
-                    # Exit loop if profitable. Return maximum bid.
-                    if maximumbid.compare( sale ) == 1 :
+                    # Exit loop if profitable.
+                    # Return maximum bid.
+                    if maximum.compare( rent ) == 1 :
                         text = f'{pair} rose {grossmargin*100}% in price. '
-                        text = text + f'It is now {maximumbid:.2f} on Gemini. '
-                        text = text + f'Selling above {sale:.2f} is profitable.'
+                        text = text + f'It is now {maximum:.2f} on Gemini. '
+                        text = text + f'Selling above {rent:.2f} is profitable.'
                         logger.info( text )
                         smsalert( text )
                         ws.close()
-                        cost.setvalue( maximumbid )
                         break
 
     # Return value when profitable only.
-    if cost.getvalue() == 0 : return False
-    else: return cost.getvalue()
+    if maximum.compare(0) == 1 : return maximum
+    else: return False
