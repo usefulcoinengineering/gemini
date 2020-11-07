aa1ce863cf3e19911c0b70e68140ac76c32ffc7d
mitzvah capital <munair@mitzvah.capital>
2020-11-07 14:21:55 -0600

    insufficient funds.

--- strategies/discountethusdquotabid.bash
+++ strategies/discountethusdquotabid.bash
@@ -10,4 +10,4 @@
 # Parameter 0 is the pair.
 # Parameter 1 is the budget (100 is 100 USD).
 # Parameter 2 is the discount (0.02 is a 2% discount).
-python3 ../discountquotafrontrunningbid.py ETHUSD 17000.03 0.001 && echo success!
+python3 ../discountquotafrontrunningbid.py ETHUSD 17000.02 0.002 && echo success!
