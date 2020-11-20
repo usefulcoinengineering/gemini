064ad40a4ad494f2f1edbfaee4e545871d8af674
mitzvah capital <munair@mitzvah.capital>
2020-11-20 16:08:49 -0600

    get 500 more!

--- strategies/discountethusdquotabid.bash
+++ strategies/discountethusdquotabid.bash
@@ -6,7 +6,6 @@
 # script purpose: bid on ETH using USD on quota
 
 # LONG on ETH and SHORT on USD, where:
-# LONG on ETH and SHORT on USD, where:
 # Parameter 0 is the pair.
 # Parameter 1 is the budget (100 is 100 USD).
 # Parameter 2 is the discount (0.02 is a 2% discount).
