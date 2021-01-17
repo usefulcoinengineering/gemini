946e74e89e9c61f83b2eebb7cd5f7396e73dd873
munair <mnair@gmail.com>
2021-01-17 11:16:35 -0600

    Add quotaask to library for liquiditymaker.

--- libraries/liquiditymaker.py
+++ libraries/liquiditymaker.py
@@ -115,3 +115,50 @@ def askorder(
     response = requests.post(request, data = None, headers = headers['restheader'])
 
     return response
+
+def quotaask(
+        pair: str,
+        cash: str,
+        cost: str
+    ) -> None:
+
+    # Determine API transaction fee.
+    # Refer to https://docs.gemini.com/rest-api/#basis-point.
+    # Fees are calculated on the notional value of each trade (price × size).
+    # Meaning (for API transactions): size * price * 1.001 = cash
+    fraction = Decimal( constants.apitransactionfee )
+    notional = Decimal(cash) / Decimal( 1 + fraction )
+
+    # Determine minimum order size (let's call it a tock).
+    list = constants.minimumorders
+    item = [ item['minimumorder'] for item in list if item['currency'] == pair[:3] ]
+    tock = Decimal( item[0] )
+
+    # Determine bid size.
+    quantity = str( Decimal( notional / Decimal(cost) ).quantize( tock ) )
+    askprice = str(cost)
+
+    # Update logs.
+    logger.debug(f'askprice: {askprice}')
+    logger.debug(f'quantity: {quantity}')
+
+    # Construct sell order payload.
+    # Use 'options': ['maker-or-cancel'] for post only orders.
+    endpoint = '/v1/order/new'
+    t = datetime.datetime.now()
+    payload = {
+        'request': endpoint,
+        'nonce': str(int(time.mktime(t.timetuple())*1000)),
+        'symbol': pair,
+        'amount': quantity,
+        'price': askprice,
+        'side': 'sell',
+        'type': 'exchange limit',
+        'options': ['maker-or-cancel']
+    }
+    headers = authenticator.authenticate(payload)
+
+    request = resourcelocator.restserver + endpoint
+    response = requests.post(request, data = None, headers = headers['restheader'])
+
+    return response
