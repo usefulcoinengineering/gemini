34f3f08a282fe51ee3b013449a7c1f28961889e3
munair <munair@gmail.com>
2021-02-25 10:23:32 -0600

    Accumulating BTC.

--- strategies/skimbtcusd.bash
+++ strategies/skimbtcusd.bash
@@ -5,7 +5,7 @@
 # script created: 20201010
 # script purpose: bid and ask on BTC using USD on quota
 
-# LONG on BTC and accumulating BTC:
+# LONG on BTC and accumulating gains in BTC:
 # Parameter 0 is the pair.
 # Parameter 1 is the budget (100 is 100 USD).
 # Parameter 2 is the discount (0.02 is a 2% discount).
