fa8b2bd33a73b0838cf1091c52258ddeb9eed285
munair <munair@gmail.com>
2021-02-25 13:39:32 -0600

    Update documentation.

--- strategies/skimbtcusd.bash
+++ strategies/skimbtcusd.bash
@@ -8,7 +8,8 @@
 # LONG on BTC and accumulating gains in BTC:
 # Parameter 0 is the pair.
 # Parameter 1 is the budget (100 is 100 USD).
-# Parameter 2 is the discount (0.02 is a 2% discount).
+# Parameter 2 is the discount (0.003 is a 0.3% discount on the market price).
+# Parameter 3 is the premium (0.007 is a 0.7% premium on the purchase price).
 while [[ True ]]; do
-  python3 ../quotafrontrunningskim.py BTCUSD 1378 0.003 0.003
+  python3 ../quotafrontrunningskim.py BTCUSD 1378 0.003 0.0076
 done
