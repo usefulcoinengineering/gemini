c5006c5e0d8295cd271e4c7a89ad117b52ae602e
munair <munair@gmail.com>
2021-03-03 08:52:40 -0600

    smaller amount

--- strategies/premiumbtcusdask.bash
+++ strategies/premiumbtcusdask.bash
@@ -9,4 +9,4 @@
 # Parameter 0 is the pair.
 # Parameter 1 is the order size in terms of the base currency (0.07765 is 0.07765 BTC).
 # Parameter 2 is the premium (0.03 is a 3% premium).
-python3 ../premiumfrontrunningask.py BTCUSD 0.32468703 0.03 && echo success!
+python3 ../premiumfrontrunningask.py BTCUSD 0.00032468 0.03 && echo success!
