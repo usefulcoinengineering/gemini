7d8674b96c87b04d6139edbb5e23e426f5eb1104
munair <munair@gmail.com>
2021-03-03 08:51:55 -0600

    testing

--- strategies/premiumbtcusdask.bash
+++ strategies/premiumbtcusdask.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# script name: discountbtcusdask.bash
+# script name: premiumbtcusdask.bash
 # script author: munair simpson
 # script created: 20210303
 # script purpose: (base currency) ask for USD (providing BTC)
@@ -8,5 +8,5 @@
 # SHORT on BTC and LONG on USD, where:
 # Parameter 0 is the pair.
 # Parameter 1 is the order size in terms of the base currency (0.07765 is 0.07765 BTC).
-# Parameter 2 is the premium (0.02 is a 2% premium).
-python3 ../discountfrontrunningask.py BTCUSD 0.07765 0.01 && echo success!
+# Parameter 2 is the premium (0.03 is a 3% premium).
+python3 ../premiumfrontrunningask.py BTCUSD 0.32468703 0.03 && echo success!
--- strategies/premiumyfiusdask.bash
+++ strategies/premiumyfiusdask.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# script name: discountyfiusdask.bash
+# script name: premiumyfiusdask.bash
 # script author: munair simpson
 # script created: 20201031
 # script purpose: ask on YFI using USD on quota
@@ -10,4 +10,4 @@
 # Parameter 0 is the pair.
 # Parameter 1 is the order size in terms of the base currency (0.07765 is 0.07765 YFI).
 # Parameter 2 is the discount (0.02 is a 2% discount).
-python3 ../discountfrontrunningask.py YFIUSD 0.07765 0.01 && echo success!
+python3 ../premiumfrontrunningask.py YFIUSD 0.07765 0.01 && echo success!
