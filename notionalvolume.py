471d7f97ca8f8428d00f039be694f4c7d03e5bb3
munair <munair@gmail.com>
2021-03-07 14:19:23 -0600

    test all combinations.

--- notionalvolume.py
+++ notionalvolume.py
@@ -10,12 +10,14 @@
 import sys
 import json
 import requests
+import locale
 
 from libraries.logger import logger
 from libraries.volumizer import notionalvolume
 
 
-# Set bid size ['0.1' is the minimum for DAIUSD].
+# Set locale and default field value.
+locale.setlocale( locale.LC_ALL, '' )
 field = 'notional_30d_volume'
 
 # Override defaults with command line parameters.
@@ -27,12 +29,18 @@ logger.debug(f'submitting request...')
 post = notionalvolume()
 
 # Display desired field (or everything received).
-if field == '': post = post.json()
-else: post = post.json()[field]
+if field == '':
+    post = post.json()
 
-# Format response.
-dump = json.dumps( post, sort_keys=True, indent=4, separators=(',', ': ') )
-logger.info ( dump )
+    # Format response.
+    dump = json.dumps( post, sort_keys=True, indent=4, separators=(',', ': ') )
+    logger.info ( dump )
+
+else:
+    post = post.json()[field]
+    # Format response.
+    if field == 'notional_30d_volume': print( f'notional 30-day volume is {locale.currency( post, grouping=True )}.' )
+    if field == 'api_maker_fee_bps': print( f'the fee that Gemini is charging you for making orders via the API is {post} basis points.' )
 
 # Let the shell know we successfully made it this far!
 if post: sys.exit(0)
