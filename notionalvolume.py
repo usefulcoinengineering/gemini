09daf1fad0324a220eefd70a98518ca3e48f9b47
munair <munair@gmail.com>
2021-03-07 15:12:40 -0600

    step through it.

--- notionalvolume.py
+++ notionalvolume.py
@@ -32,10 +32,6 @@ dump = json.dumps( data, sort_keys=True, indent=4, separators=(',', ': ') )
 
 # Format response.
 if field == '': logger.debug ( dump )
-else:
-    if field == 'notional_30d_volume': print( f'your notional 30-day volume is {post.json()[field]:,} USD.' )
-    else:
-        if field == 'api_maker_fee_bps': print( f'the fee that Gemini is charging you for making orders via the API is {post.json()[field]} basis points.' )
 
 
 # Let the shell know we successfully made it this far!
