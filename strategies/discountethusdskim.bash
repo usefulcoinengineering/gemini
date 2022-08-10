2074e37a02f831095aff49faa0df4c52d2c234be
munair <munair@gmail.com>
2022-08-10 10:30:30 -0700

    add cautionary note for parameter setting.

--- strategies/discountethusdskim.bash
+++ strategies/discountethusdskim.bash
@@ -13,6 +13,9 @@
 # Parameter 2 is the discount (0.003 is a 0.3% discount on the market price).
 # Parameter 3 is the premium (0.007 is a 0.7% premium on the purchase price).
 
+# Bullish Market Trend: make the premium more than the discount.
+# Bearish Market Trend: make the discount more than the premium.
+
 # Production
 # python3 ../discountfrontrunningskim.py ETHUSD 20 0.0376 0.0378
 
@@ -22,6 +25,11 @@
 # Big Dipper Testing
 # python3 ../discountfrontrunningskim.py ETHUSD 1 0.076 0.078
 
+# Gemini's API Transaction Fee
+# Presently: 20 basis points (or '0.002').
+# Premium Impact: Always add 0.002 to the premium desired (in order to offset the fee for selling).
+# Discount Impact: Always subtract 0.002 from the discount desired (in order to offset the fee for buying).
+
 while [[ True ]]; do
-  python3 ../discountfrontrunningskim.py ETHUSD 0.005 0.0013 0.0013
+  python3 ../discountfrontrunningskim.py ETHUSD 0.005 0.0037 0.0037
 done
