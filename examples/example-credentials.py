cce86b865989cfed80e846b52486ee33518c0a91
munair <munair@gmail.com>
2022-08-10 09:46:27 -0700

    update example file with better best practices.

--- examples/example-credentials.py
+++ examples/example-credentials.py
@@ -9,3 +9,7 @@
 name = 'tester'
 key = 'account-wCyxf41Q3Ytavlkx0CSs'
 secret = '2rmFxuo+XnZG.zB8@mgHvR29zS1c=='
+
+# Webhooks for Discord should be private to prevent spamming from bots. So put them here -
+
+discordwebhook = 'https://discord.com/api/webhooks/1006964160514502888/9I0BJ5kReoZ0NPGvwJklrHiivh12_sYe9wSJzht-wyWJJ1ilAMQs7y0TDBxFKpqyt_mO'
