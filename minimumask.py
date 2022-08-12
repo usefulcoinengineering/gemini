ac93322efdeebcfdfc17fc9380b155183862343a
munair <munair@gmail.com>
2022-08-11 21:06:26 -0700

    add test for the lowest ask

---
+++ minimumask.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#
+# test name: minimumask.py
+# test author: munair simpson
+# test created: 20220811
+# test purpose: retrieve public market data on the lowest ask in the orderbook using Gemini's REST API.
+
+
+# Detailed Description:
+#  1. Use the informer library to request the required information.
+#  2. Print the results using the logger library.
+#  3. Send an alert to a Discord channel using the messenger library's webhook.
+#
+# Execution:
+#   - Use the wrapper BASH script in the "tests" directory.
+
+import sys
+
+from libraries.logger import logger
+from libraries.informer import minimumask
+from libraries.messenger import appalert as appalert
+
+
+# Set trading default trading pair in cause a BASH wrapper has not been used.
+pair = 'BTCUSD'
+
+# Override defaults with command line parameters from BASH wrapper.
+if len(sys.argv) == 2:
+    pair = sys.argv[1]
+
+# Get public market data on the lowest ask in the orderbook using the Gemini REST API.
+cost = minimumask( pair )
+
+# Report the response if there is one.
+if cost:
+    # Tell the user the result.
+    fragmentone = f'The lowest ask for {pair[:3]} in the Gemini orderbook is: '
+    fragmenttwo = f'{cost} {pair[3:]}.'
+    logger.info ( f'{fragmentone}{fragmenttwo}')
+    appalert ( f'{fragmentone}{fragmenttwo}')
+
+    # Let the shell know we successfully made it this far!
+    sys.exit(0)
+
+else: sys.exit(1)
---
+++ tests/minimumask.bash
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# script name: minimumask.bash
+# script author: munair simpson
+# script created: 20220811
+# script purpose: wrapper for minimumask.py
+
+# Retrieve public market data on the lowest ask in the orderbook using Gemini's REST API:
+# Parameter 0 is the pair.
+
+# Execution:
+# python3 ../minimumask.py ENSUSD
+
+python3 ../minimumask.py ENSUSD
