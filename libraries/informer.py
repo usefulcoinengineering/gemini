4556c5c8e9597543ee25871e037541da26a93661
munair <munair@gmail.com>
2022-08-11 18:41:30 -0700

    add functions to return the highest bid and the lowest ask in the orderbooks.

---
+++ libraries/informer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#
+# library name: informer.py
+# library author: munair simpson
+# library created: 20220811
+# library purpose: retrieve market data.
+
+
+import requests
+import ssl
+import json
+import datetime
+import time
+
+from decimal import Decimal
+
+from libraries.logger import logger as logger
+
+import libraries.constants as constants
+import libraries.authenticator as authenticator
+import libraries.resourcelocator as resourcelocator
+
+def maximumbid(
+        pair: str
+    ) -> None:
+
+    # Get the highest bid in the orderbook.
+    endpoint = '/v1/pubticker/' + pair
+    response = requests.get( resourcelocator.restserver + endpoint )
+    bidprice = Decimal( response.json()['bid'] )
+
+    # Update logs.
+    logger.debug(f'bidprice: {bidprice}')
+
+    return bidprice
+
+def minimumask(
+        pair: str
+    ) -> None:
+
+    # Get the lowest ask in the orderbook.
+    endpoint = '/v1/pubticker/' + pair
+    response = requests.get( resourcelocator.restserver + endpoint )
+    askprice = Decimal( response.json()['ask'] )
+
+    # Update logs.
+    logger.debug(f'askprice: {askprice}')
+
+    return askprice
