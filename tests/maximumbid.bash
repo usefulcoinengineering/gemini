f9ef798427c6cec8116b4a0ba15d3fac5b7ae434
munair <munair@gmail.com>
2022-08-11 21:05:38 -0700

    correct misinformation again

--- tests/maximumbid.bash
+++ tests/maximumbid.bash
@@ -5,7 +5,7 @@
 # script created: 20220811
 # script purpose: wrapper for maximumbid.py
 
-# Retrieve public market data on the highest ask in the orderbook using Gemini's REST API:
+# Retrieve public market data on the highest bid in the orderbook using Gemini's REST API:
 # Parameter 0 is the pair.
 
 # Execution:
