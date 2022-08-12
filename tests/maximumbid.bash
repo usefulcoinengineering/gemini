97f015df6e3134ebacf6878dd5d8e25944c772f4
munair <munair@gmail.com>
2022-08-11 19:07:07 -0700

    write tests for and then test the new library

---
+++ tests/maximumbid.bash
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# script name: maximumbid.bash
+# script author: munair simpson
+# script created: 20220811
+# script purpose: wrapper for maximumbid.py
+
+# Retrieve public market data on the highest ask in the orderbook using Gemini's REST API:
+# Parameter 0 is the pair.
+
+# Execution:
+# python3 ../maximumbid.py ENSUSD
+
+python3 ../maximumbid.py ENSUSD
