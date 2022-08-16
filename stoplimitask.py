443ac166b29d7dcde6e41ccdd15f859ca7eb5597
munair <munair@gmail.com>
2022-08-15 17:31:27 -0700

    experimentation

--- stoplimitask.py
+++ stoplimitask.py
@@ -50,9 +50,9 @@ if Decimal(sell).compare( Decimal(stop) ) == 1:
 
 # Get public market data on the lowest ask in the orderbook using the Gemini REST API.
 sale = limitstop( pair, size, stop, sell )
-if sale.status == 400 :
-    logger.debug ( "Error encountered.")
 sale = sale.json()
+if sale['status'] == 400 :
+    logger.debug ( "Error encountered.")
 dump = json.dumps( sale, sort_keys=True, indent=4, separators=(',', ': ') )
 logger.debug ( dump )
 
