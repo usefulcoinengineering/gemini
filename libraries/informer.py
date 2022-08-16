1e52cfd1773ef0e2843566799b7e65f7e029290c
munair <munair@gmail.com>
2022-08-15 23:34:39 -0700

    give informer a makeover.

--- libraries/informer.py
+++ libraries/informer.py
@@ -7,14 +7,12 @@
 
 
 import requests
-import ssl
 import json
-import datetime
-import time
 
 from decimal import Decimal
 
 from libraries.logger import logger as logger
+from libraries.messenger import appalert as appalert
 
 import libraries.definer as definer
 import libraries.authenticator as authenticator
@@ -26,12 +24,23 @@ def maximumbid(
     # Get the highest bid in the orderbook.
     endpoint = '/v1/pubticker/' + pair
     response = requests.get( definer.restserver + endpoint )
-    bidprice = Decimal( response.json()['bid'] )
+    datadump = json.dumps( response, sort_keys=True, indent=4, separators=(',', ': ') )
 
-    # Update logs.
-    logger.debug(f'bidprice: {bidprice}')
+    # Write the dump to logs.
+    logger.debug ( datadump )
 
-    return bidprice
+    # Send notice if error arises.
+    if datadump["result"] == "error" :
+        appalert ( f'\"{datadump["reason"]}\" {datadump["result"]}: {datadump["message"]}' )
+
+        # Exit returning a boolean value of "False".
+        return False
+
+    else:
+        # Update logs and return bid price as a string.
+        bidprice = Decimal( response.json()['bid'] )
+        logger.debug( f'bidprice: {bidprice}' )
+        return str(bidprice)
 
 def minimumask(
         pair: str
@@ -40,9 +49,21 @@ def minimumask(
     # Get the lowest ask in the orderbook.
     endpoint = '/v1/pubticker/' + pair
     response = requests.get( definer.restserver + endpoint )
-    askprice = Decimal( response.json()['ask'] )
+    datadump = json.dumps( response, sort_keys=True, indent=4, separators=(',', ': ') )
+    
+    
+    # Write the dump to logs.
+    logger.debug ( datadump )
 
-    # Update logs.
-    logger.debug(f'askprice: {askprice}')
+    # Send notice if error arises.
+    if datadump["result"] == "error" :
+        appalert ( f'\"{datadump["reason"]}\" {datadump["result"]}: {datadump["message"]}' )
 
-    return askprice
+        # Exit returning a boolean value of "False".
+        return False
+
+    else:
+        # Update logs and return ask price as a string.
+        askprice = Decimal( response.json()['ask'] )
+        logger.debug( f'askprice: {askprice}' )
+        return str(askprice)
