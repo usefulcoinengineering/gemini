a4539dff2cbceabf1fded656fb3516f6e78957d5
munair <munair@gmail.com>
2022-08-17 16:22:08 -0400

    format json

--- libraries/pricemonitor.py
+++ libraries/pricemonitor.py
@@ -6,10 +6,13 @@
 # library purpose: continually monitor trade prices via Gemini's Websockets API until the exit threshold is breached.
 # library legacy: originally "dealseeker.py" because it waited on a fall from websocket session highs.
 
-import ssl
+import ssl, json
 import websocket
 
-def on_message( ws, message ) : print(message)
+def on_message( ws, message ) : 
+    jsonresponse = message.json()
+    jsondatadump = json.dumps( jsonresponse, sort_keys=True, indent=4, separators=(',', ': ') )
+    print ( jsondatadump )
 
 ws = websocket.WebSocketApp( "wss://api.gemini.com/v1/marketdata/btcusd?trades=true", on_message = on_message )
 ws.run_forever( sslopt = { "cert_reqs" : ssl.CERT_NONE } )
