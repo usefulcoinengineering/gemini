ed594116aac6d3807e0a11b4dedc3fb3119ed721
munair <munair@gmail.com>
2022-08-19 12:29:00 -0400

    look at post response.

--- notionalvolume.py
+++ notionalvolume.py
@@ -23,7 +23,7 @@ jsonresponse = notionalvolume().json()
 
 # Remove comments to debug.
 jsondatadump = json.dumps( jsonresponse, sort_keys=True, indent=4, separators=(',', ': ') )
-# logger.debug ( jsondatadump )
+logger.debug ( jsondatadump )
 
 # Format response.
 logger.info( f'Notional 30 day Trading Volume: {jsonresponse["notional_30d_volume"]:.2f} USD. ' )
