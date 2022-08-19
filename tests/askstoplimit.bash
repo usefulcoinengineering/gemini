ba6340560c7e72d9085eaaf852f7800e56dc9931
munair <munair@gmail.com>
2022-08-19 13:31:04 -0400

    experiment with taking keyboard entries that override defaults.

--- tests/askstoplimit.bash
+++ tests/askstoplimit.bash
@@ -12,6 +12,17 @@
 # Parameter 3 is the sell.
 
 # Execution:
-# python3 ../askstoplimit.py ETHUSD
+# python3 ../askstoplimit.py ETHUSD 0.001 0.0050 0.0100
 
-python3 ../askstoplimit.py ETHUSD 0.001 0.0050 0.0100
+# Define default values.
+pair="ETHUSD"
+size="0.0001"
+stop="0.0050"
+sell="0.0100"
+
+read -p "type hostname or press enter to continue with default [$pair]: " pair && pair=${pair:-$pair}
+read -p "type hostname or press enter to continue with default [$size]: " size && size=${size:-$size}
+read -p "type hostname or press enter to continue with default [$stop]: " stop && stop=${stop:-$stopt}
+read -p "type hostname or press enter to continue with default [$sell]: " sell && sell=${sell:-$sell}
+
+python3 ../askstoplimit.py $pair $size $stop $sell
