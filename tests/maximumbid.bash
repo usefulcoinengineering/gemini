d4e41efae17b16f303711698231df3f5eff8f721
munair <munair@gmail.com>
2022-08-19 14:22:32 -0400

    add command line argment entry support.

--- tests/maximumbid.bash
+++ tests/maximumbid.bash
@@ -5,10 +5,14 @@
 # script created: 20220811
 # script purpose: wrapper for maximumbid.py
 
-# Retrieve public market data on the highest bid in the orderbook using Gemini's REST API:
+# Retrieve public market data on the highest bid in the pairbook using Gemini's REST API:
 # Parameter 0 is the pair.
 
 # Execution:
 # python3 ../maximumbid.py ENSUSD
 
-python3 ../maximumbid.py ENSUSD
+pair="ETHUSD"
+
+read -p "type a replacement value or press enter to continue with default argument [$pair]: " pair && pair=${pair:-136457975606}
+
+python3 ../maximumbid.py $pair
