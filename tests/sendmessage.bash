4f27fe4f91df15b7cebce518edf26c0f24f94fdd
munair <munair@gmail.com>
2022-08-19 14:07:50 -0400

    try single quotes

--- tests/sendmessage.bash
+++ tests/sendmessage.bash
@@ -11,9 +11,9 @@
 # Execution:
 # python3 ../sendmessage.py "Sending a test message from a BASH script."
 
-message="Sending a test message from a BASH script that is a wrapper around a Python script (sendmessage.py)."
+message="\"Sending a test message from a BASH script that is a wrapper around a Python script (sendmessage.py).\""
 
 read -p "type a replacement value or press enter to continue with default argument [$message]: " message
-message=${message:-"Sending a test message from a BASH script that is a wrapper around a Python script (sendmessage.py)."}
+message=${message:-'Sending a test message from a BASH script that is a wrapper around a Python script (sendmessage.py).'}
 
 python3 ../sendmessage.py $message
