9f6b918b254e414fcac3e30bc325d8888106d351
munair <munair@gmail.com>
2022-08-27 22:24:20 -0400

    use a script to safely stop and start bots

---
+++ scripts/gemini-ethusd-trailer.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# 
+# script name: gemini-ethusd-trailer.bash
+# script author: munair simpson
+# script created: 20220827
+# script purpose: start or stop a Gemini ETHUSD trailer bot from [~/bots]
+# script argument: the action desired [start/stop]
+
+ps auwx | grep -E 'bash [bf]|python3 \.' && echo "there are active bots running. "
+
+read -p "'start' or 'stop' trading bot [start]: " action
+action=${action:-start}
+
+if [ $action == "stop" ]; then kill -s KILL $(ps auwx | grep -E 'bash [bf]|python3 \.' | awk '{print $2}') ; fi
+if [ $action == "start" ]; then cd gemini/strategies/ && git pull && nohup bash frontrunningethusdtrailingstop.bash ; fi
