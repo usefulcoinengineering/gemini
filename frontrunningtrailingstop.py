fa62775dbe5a54afac7861d89e9ffde2001c1151
munair <munair@gmail.com>
2022-08-29 18:09:54 -0400

    just remove it altogether

--- frontrunningtrailingstop.py
+++ frontrunningtrailingstop.py
@@ -365,10 +365,6 @@ while True : # Block until prices rise (then cancel and resubmit stop limit orde
             try:
                 
                 if jsonresponse['is_live'] : 
-                    order = jsonresponse['order_id']
-                    price = jsonresponse['price']
-                    infomessage = f'Updated {price} stop limit order {order} is live on the Gemini orderbook. '
-                    logger.info ( infomessage )
                     break # Break out of the while loop because the stop order was executed and we now want to block until prices rise.
                 else : 
                     logger.info ( 'An error occurred. Unable to submit a stop limit order. ' )
