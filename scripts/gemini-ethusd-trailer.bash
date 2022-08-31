afce7cf7eb942b66c6203e79f00ca31bdb4e1082
munair <munair@gmail.com>
2022-08-30 20:44:40 -0400

    simplify startup script.

--- scripts/gemini-ethusd-trailer.bash
+++ scripts/gemini-ethusd-trailer.bash
@@ -6,43 +6,14 @@
 # script purpose: start or stop a Gemini ETHUSD trailer bot from [~/bots]
 # script argument: the action desired [start/stop]
 
-if [ "$#" != "1" ]; then
+# step 1: check for bots
+ps auwx | grep -e "bash [bf]" -e "python3 \." | grep -v "$0" && echo "There is at least one active bot running. "
 
-    # step 1: explain error
-    echo "You should have provided either 'start' or 'stop' as an argument. "
-    echo "Not to worry... Please respond to the questions that follow. "
+# step 2: request directive
+read -p "start/stop trading bot [start]: " action
+action=${action:-"start"} 
 
-    # step 2: ask about checking for existing detached processes
-    read -p "Would you like to check for the presence of running bots? [yes]: " pingerprompt
-    pingerprompt=${pingerprompt:-"yes"}
-    if [ $pingerprompt == "yes" ]; then 
-        ps auwx | grep -e "bash [bf]" -e "python3 \." | grep -v "$0" && echo "There are active bots running. "
-        if [ $? == "1" ]; then 
-            echo "No active bots found... "
-        else
-
-            # step 3: ask how to handle detached processes since they must exist
-            read -p "Do you want to kill these running bots? [yes]: " killerprompt
-            killerprompt=${killerprompt:-"yes"}
-            if [ $pingerprompt == "yes" ]; then action="stop" ; fi        
-        fi
-    fi
-
-    # step 4: ask whether to bootstrap the bot
-    read -p "Do you want to start/boot up a bot? [yes]: " booterprompt
-    booterprompt=${booterprompt:-"yes"}
-    if [ $pingerprompt == "yes" ]; then action="start" ; fi   
-else
-    action=$1
-fi
-
-# step 5: verify
-ps auwx | grep -e "bash [bf]" -e "python3 \." | \
-grep -v "$0" && echo "There are active bots running. " && \
-read -p "Proceed? [no]" verifyprompt && verifyprompt=${verifyprompt-"no"} && \
-if [ $verifyprompt == "no" ]; then exit 1 ; fi
-
-# step 6: start or stop bot
+# step 3: execute directive
 if [ $action == "stop" ]; then kill -s KILL $(ps auwx | grep -E 'bash [bf]|python3 \.' | awk '{print $2}') ; fi
 if [ $action == "start" ]; then cd gemini/strategies/ && git pull && nohup bash frontrunningethusdtrailingstop.bash ; fi
 
